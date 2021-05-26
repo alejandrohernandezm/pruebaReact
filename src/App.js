e543f681cc17b89cf838a7cbbfba4eacae85c89c
Alejandro <alejandro.hernandez@newshore.es>
2021-05-26 14:29:42 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+
+//Components
+import ApiComponent from './Components/Api'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const url = 'https://jsonplaceholder.typicode.com/users/1/todos';
+        const [todos, setTodos] = useState()
+        const fetchApi = async () => {
+        const  response = await fetch(url)
+            console.log(response.status)
+            const json = await response.json()
+            setTodos(json);
+            console.log(json)
+        }
+
+
+        useEffect(() => {
+            fetchApi()
+        }, [])
+
+        return(
+         <div className="App">
+           <ul>
+             <h1>Api</h1>
+             {!todos ? 'Loading..': todos.map((data)=>{
+               return <li>{data.id}: {data.title}  {data.completed}</li>
+
+             })
+             }
+           </ul>
+         </div>
+        )
 }
 
 export default App;
---
+++ src/Components/Api.js
@@ -0,0 +1,18 @@
+
+import React, { useEffect, useState } from 'react';
+
+class ApiComponent extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            usuarios: [],
+        }
+
+    }
+    
+   
+
+}
+
+export default ApiComponent;
